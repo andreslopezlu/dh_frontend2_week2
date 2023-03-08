dc66f68eb688630f22fea2337e168a6899a00ac3
andreslopezlu <andreslopezlu@gmail.com>
2023-03-07 19:13:50 -0500

    consigna mesa 3 resuelto

--- clase4.js
+++ clase4.js
@@ -43,3 +43,53 @@ articulos.forEach( articulo =>{
 // 2 - Plasmarlo en un diagrama de árbol como la consigna: https://docs.google.com/document/d/15nGvKkEcbrRgwqV50ISh0QYZ_TO67vmWQaLfNpUxqjs/preview
 
 
+// Aqui se resuelve el ejercicio 1 en algunos de los nodos del DOM
+let head = document.querySelector('head');
+// console.log(head);
+let body = document.querySelector('body');
+// console.log(body);
+let h1 = document.querySelector('h1');
+// console.log(h1);
+let nav = document.querySelector('nav');
+// console.log(nav);
+let ul = document.querySelector('ul');
+// console.log(ul);
+let lista = document.querySelectorAll('ul li');
+// console.log(lista);
+let tema = document.querySelector('.tema');
+// console.log(tema);
+let clima = document.querySelector('.info .clima');
+// console.log(clima);
+let dolar = document.querySelector('.info .dolar');
+// console.log(dolar);
+let articulo = document.querySelectorAll('article');
+// console.log(articulo);
+
+
+// Se revisa la diferencia entre innerHTML e innerText: el primero retorna todo el codigo HTML dentro del nodo al que se aplica, el segundo retorna el contenido dentro de las etiquetas
+articulo.forEach(art => {
+    console.log(art.innerHTML)
+    console.log(art.innerText)
+})
+
+
+// Esta funcion asigna un nuevo titulo h1 a la pagina
+let nuevoTitulo = `<h1>Semala</h1>`;
+function asignarNuevoTitulo (template){
+    let titulo = document.querySelector('h1');
+    // let html = titulo.innerHTML = template;
+    titulo.innerText = template;
+    console.log(text)
+}
+// asignarNuevoTitulo(nuevoTitulo);
+
+
+// esta funcion asigna un nuevo titulo con el formato 'Titulo 1'
+let subtitulos = document.querySelectorAll('h2')
+let contador = 0;
+subtitulos.forEach(subtitulo => {
+    let nuevoSubtitulo = `Titulo ${contador}`
+    subtitulo.innerText = nuevoSubtitulo
+    contador++;
+})
+
